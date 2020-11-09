927d83c7793ab210d809b724e0d4553da27685a4
chqiu6 <chucky-qiu@lambdastudents.com>
2020-11-08 20:58:04 -0500

    Bubble page component done

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
@@ -8,6 +9,17 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+    .get("http://localhost:5000/api/colors")
+    .then(res => {
+      console.log("useEffect fetching colors data from server when component mounts : ", res);
+      setColorList(res.data);
+    })
+    .catch(err => {
+      console.log("useEffect fetching colors data from server when component mounts failed ! Try again : ", err);
+    });
+  },[]);
 
   return (
     <>
